fe8955d646d095f0f6d1c77d6f6e77d16d8dc53d
Mateusz Krzak <madeye@o2.pl>
2015-05-13 04:56:10 +0200

    add editing news by modal

--- js/controllers.js
+++ js/controllers.js
@@ -2,7 +2,8 @@
 
     var app = angular.module('szuszApp-adminControllers', ['ngResource','ui.bootstrap']);
 
-    app.controller('NewsController', ['$scope','$http', function ($scope, $http) {
+
+    app.controller('NewsController', ['$scope','$http', '$modal', '$log', function ($scope, $http, $modal, $log) {
         $scope.news = [];
 
         $http.get('data/news.json')
@@ -28,31 +29,73 @@
             return (begin <= index && index < end);
         };
 
-
         $scope.content ='Kliknij aby zacząć pisać swoją wiadomość...';
 
 
+        $scope.animationsEnabled = true;
 
-        $scope.showAddRow = function(){
-            $scope.id=getMax($scope.users,"id") +1;
-            $scope.role='user';
+        $scope.editNews = function (size, message) {
+
+            var modalInstance = $modal.open({
+                animation: $scope.animationsEnabled,
+                templateUrl: 'views/admin/news-edit-modal.html',
+                controller: function($scope, $modalInstance){
+
+                    $scope.save = function () {
+                        message = $scope.content;
+                        $modalInstance.close($scope.content);
+                    };
+
+                    $scope.cancel = function () {
+                        $modalInstance.dismiss('cancel');
+                    };
+
+                    $scope.news_id= message.news_id;
+                    $scope.content = message.content;
+                    $scope.firstname = message.firstname;
+                    $scope.lastname = message.lastname;
+                    $scope.author_id = message.author_id;
+                    $scope.news_date = message.news_date;
+                },
+                size: size
+
+            });
+
+            modalInstance.result.then(function (message) {
+                console.log(message);
+                $log.info('Modal  at: ' + new Date());
+            }, function () {
+                $log.info('Modal dismissed at: ' + new Date());
+            });
         };
 
-        $scope.addRow = function(){
-            $scope.users.push({ 'id':$scope.id, 'firstname': $scope.firstname, 'lastname': $scope.lastname, 'email': $scope.email, 'role':$scope.role });
-            $scope.id='';
+
+        $scope.addNews = function(){
+            $scope.news_id = getMax($scope.news,"news_id")+1;
+            $scope.news_date = new Date();
+
+            $scope.news.push({
+                'news_id':$scope.news_id,
+                'content': $scope.content,
+                'firstname': $scope.firstname,
+                'lastname': $scope.lastname,
+                'author_id': $scope.author_id,
+                'news_date':$scope.news_date
+            });
+            $scope.news_id='';
+            $scope.content='';
             $scope.firstname='';
             $scope.lastname='';
-            $scope.email='';
-            $scope.role='';
-            postUsers();
+            $scope.author_id='';
+            $scope.news_date='';
+            //postUsers();
         };
 
-        $scope.removeRow = function(user_id){
+        $scope.removeNews = function(news_id){
             var index = -1;
-            var comArr = eval( $scope.users );
+            var comArr = eval( $scope.news );
             for( var i = 0; i < comArr.length; i++ ) {
-                if( comArr[i].id === user_id ) {
+                if( comArr[i].news_id === news_id ) {
                     index = i;
                     break;
                 }
@@ -60,15 +103,19 @@
             if( index === -1 ) {
                 alert( "Błąd w funkcji removeRow" );
             }
-            $scope.users.splice( index, 1 );
+            $scope.news.splice( index, 1 );
         };
 
-
+        function getMax(tab, field) {//zwraca max z tab pola field
+            var max;
+            for (var i=0 ; i<tab.length ; i++) {
+                if (!max || parseInt(tab[i][field]) > parseInt(max[field]))
+                    max = tab[i];
+            }
+            return max[field];
+        }
 
     }]); //NewsController
-
-
-
 
     app.controller('UsersController', ['$scope','$http', function ($scope, $http) {
         $scope.users = [];
@@ -173,8 +220,6 @@
 
 
     }]); //UsersController
-
-
 
 })();
 
